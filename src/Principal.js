0bb11d861301a06fa4b57e8525f8527ae95b7b90
Jordan <erick_jstm@hotmail.com>
2024-04-12 20:44:24 -0300

    Novo

--- src/Principal.js
+++ src/Principal.js
@@ -6,11 +6,11 @@ import cftv5 from  './Imagens/CFTV5.jpg'
 import Menu from "./Menu"
 function Principal(){
    const data = [
-      {Nome:'Cameras de monitoramento predial',img :cftv1},
-      {Nome:'Cameras Internas',img:cftv2},
-      {Nome:'Cercas eletrificadas',img:cftv4},
-      {Nome:'Manutenções em instalações',img:cftv5},
-      {Nome:'Porteiro Eletronico',img:cftv5},
+      {Nome:'Cameras de monitoramento predial',img :cftv1,Texto:'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'},
+      {Nome:'Cameras Internas',img:cftv2,Texto:'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'},
+      {Nome:'Cercas eletrificadas',img:cftv4,Texto:'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'},
+      {Nome:'Manutenções em instalações',img:cftv5,Texto:'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'},
+      {Nome:'Porteiro Eletronico',img:cftv5,Texto:'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.'},
    ]
    return(
     <div className="geral">
@@ -21,6 +21,7 @@ function Principal(){
              {data.map((item)=>(
             <div className="container"><h3 className="titulo">{item.Nome}</h3>
              <img className="img" alt={item.Nome} src={item.img}></img>
+             <p className="texto">{item.Texto}</p>
             </div>
              ))}
            </h1>
