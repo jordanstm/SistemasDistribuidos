f66eb35e03d72a16c58917ad44e7c15eca928640
Jordan <erick_jstm@hotmail.com>
2024-04-24 15:32:35 -0300

    Ajuste Menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,11 @@
 import './menu.css'
 import shop from './Imagens/shopCart.png'
-import { useNavigate} from 'react-router-dom'
+import { useNavigate,Navigate} from 'react-router-dom'
 import { useEffect,useState } from 'react';
 function Menu({total}){
 
    
-  
+  const [nome,setNome]=useState()
     const [listar,SetListar]= useState(false)
     const nav = useNavigate();
     useEffect(()=>{
@@ -18,16 +18,17 @@ function Menu({total}){
     return(
 
         <div className="menu">
-                <a href="\">Principal</a>
-                    <a href="\">Produtos</a>
-                    <a href="\">Login</a>                 
+                <button  onClick={()=>{setNome('/Principal')}}>Principal</button>
+                    <button onClick={()=>{setNome('/ListarCarrinho')}}>Produtos</button>
+                    <button  onClick={()=>{setNome('/')}}>Login</button>                 
+                    <button  onClick={()=>{setNome('/CadastroUsuario')}}>CadastroUsuario</button>                 
                     
                       <div className='containerCard'>
                        <div className='Total'>{total=6}</div>    
                       <img className='ima' src={shop} onClick={()=>{SetListar(true)}} alt='CarShop'></img>
                       </div>
                        
-                    
+                    {nome && (<Navigate to={nome}></Navigate>)}
                             
                         
                                 
